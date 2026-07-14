8893de1c359f176cf11667054f1539eb9cb325c4
Abuzarr123 <120338858+Abuzarr123@users.noreply.github.com>
2026-07-14 13:42:36 +0000

    reordering

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from azure.monitor.opentelemetry import configure_azure_monitor
+import logging
+
+logging.basicConfig(level=logging.INFO)
 configure_azure_monitor()
 
 from flask import Flask, render_template, request
 from datetime import datetime, timezone
-import logging
 from werkzeug.utils import redirect
 from flask_config import Config
 from data.database import initialise_database, add_order, clear_orders, count_orders, get_orders_to_display, get_queued_count, get_recently_placed_count, get_recently_processed_count
@@ -11,13 +13,13 @@ from scheduled_jobs import initialise_scheduled_jobs
 from products import create_product_download
 import requests
 
-logging.basicConfig(level=logging.INFO)
 app = Flask(__name__)
 app.config.from_object(Config)
 
 initialise_database(app)
 initialise_scheduled_jobs(app)
 
+# ... routes unchanged ...
 
 @app.route("/")
 def index():
